dbe9512bc82a9f136cd79c487c7789aab1543414
Zachary Bushby <zachbush678@gmail.com>
2023-11-09 19:05:29 +1100

    .

--- Cleaning/3. Cleaning.R
+++ Cleaning/3. Cleaning.R
@@ -1,11 +1,26 @@
 
 #Scale time and amount
+data 
+
+# Assuming your data frame is named 'df' and the columns you want to scale are 'column1' and 'column2'
+df$column1_scaled <- scale(df$column1, center = TRUE, scale = TRUE)
+df$column2_scaled <- scale(df$column2, center = TRUE, scale = TRUE)
+
+
+#examples of different sampling methods
+#undersampling: maybe remove key charateristics - naive
+#oversampling : decreasing variance - overfitting - naive
+#synthetic minority oversampling technique (SMOTE)
+#near miss algorithim
+#Tomek Links undersampling: takes 2 observations of different classes whos distance is shortest using KNN then deletes majority class
 
 
 
-#random undersampling
 
 
+#writing clean csv
 
+# Assuming your data frame is named df
+write.csv(df, file = "your_data.csv", row.names = FALSE)
 
 
--- Load Data:Libraries/1. Libraries.R
+++ Load Data:Libraries/1. Libraries.R
@@ -4,6 +4,10 @@ library(dplyr)
 library(readr)
 library(tidyr)
 library(DataExplorer)
+library(themis)
+#library(DMwR) DMwR removed from Cran :(
+library(caret)
+library(smotefamily)
 
 #load data
 data <- read_csv("//Users//zachbushby//Documents//Uni//Career//Projects//Data//creditcard.csv")
